37d57157324bae5392d9665592329bc0ce27dcef
vu tien dat <vutiendat010896@gmail.com>
2021-09-23 15:38:53 +0700

    first commit

---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+import { commerce } from '../../lib/commerce';
+import FormInput from './CustomTextField';
+
+const AddressForm = ({ checkoutToken, test }) => {
+  const [shippingCountries, setShippingCountries] = useState([]);
+  const [shippingCountry, setShippingCountry] = useState('');
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setShippingSubdivision] = useState('');
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [shippingOption, setShippingOption] = useState('');
+  const methods = useForm();
+
+  const fetchShippingCountries = async (checkoutTokenId) => {
+    const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+    setShippingCountries(countries);
+    setShippingCountry(Object.keys(countries)[0]);
+  };
+
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivision(Object.keys(subdivisions)[0]);
+  };
+
+  const fetchShippingOptions = async (checkoutTokenId, country, stateProvince = null) => {
+    const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region: stateProvince });
+
+    setShippingOptions(options);
+    setShippingOption(options[0].id);
+  };
+
+  useEffect(() => {
+    fetchShippingCountries(checkoutToken.id);
+  }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+  }, [shippingCountry]);
+
+  useEffect(() => {
+    if (shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
+  }, [shippingSubdivision]);
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>Địa chỉ nhận hàng</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit((data) => test({ ...data, shippingOption }))}>
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="Họ" />
+            <FormInput required name="lastName" label="Tên" />
+            <FormInput required name="address1" label="Địa chỉ người nhận" />
+            <FormInput required name="email" label="Số điện thoại" />
+          
+            
+            
+          </Grid>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Button component={Link} variant="outlined" to="/cart">Quay lại giỏ hàng</Button>
+            <Button type="submit" variant="contained" color="primary">Tiếp</Button>
+          </div>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
+
+export default AddressForm;
